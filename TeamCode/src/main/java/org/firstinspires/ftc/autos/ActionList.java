71c0de4a5535b6925f200b21734e8ae66e6dd8a8
Matthew <mdsflyboy@gmail.com>
2019-11-25 03:03:15 -0800

    reset and run action list

--- TeamCode/src/main/java/org/firstinspires/ftc/autos/ActionList.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/autos/ActionList.java
@@ -4,6 +4,7 @@ public class ActionList {
     Action root = null;
     Action current = null;
     Action last = null;
+    private boolean ranStart = false;
     public ActionList (Action root){
         this.root = root;
         current = root;
@@ -12,6 +13,26 @@ public class ActionList {
     public void add(Action newAction){
         last.next = newAction;
         last = newAction;
-        
+    }
+    public void reset(){
+        current = root;
+        ranStart = false;
+    }
+    public void run(){
+        if(current == null){
+            return;
+        }
+        if (!ranStart){
+            current.start();
+            ranStart = true;
+        }
+        current.update();
+        if (current.isFinished()){
+            current.done();
+            current = current.next;
+            ranStart = false;
+        }
+
+
     }
 }
