a043d417fae2451f97533ab7d5f34cf7f4a9a424
Katherine <khbgiggles@gmail.com>
2020-01-16 19:54:03 -0800

    made a state machine that moved the robot

--- TeamCode/src/main/java/org/firstinspires/ftc/autos/autoA1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/autos/autoA1.java
@@ -19,6 +19,9 @@ public class autoA1 extends OpMode {
     DcMotor eE;
 
     PID pid;
+
+    int state = 1;
+    double startTime;
     @Override
     public void init() {
         Right = hardwareMap.dcMotor.get("eRight");
@@ -30,17 +33,73 @@ public class autoA1 extends OpMode {
         eE = hardwareMap.dcMotor.get("eE");
         Hook1 = hardwareMap.servo.get("Hook1");
         Hook2 = hardwareMap.servo.get("Hook2");
-        pid = new PID(1,0.2);
-        pid.setSetpoint(10);// distance in inches
-
+        pid = new PID(0.5,0.0);
+        pid.setSetpoint(32);// distanc+e in inches
     }
 
     @Override
     public void loop() {
-        double avePos = (Right.getCurrentPosition() + Left.getCurrentPosition())/2;// get avg position of the motors
+        double avePos = (-Right.getCurrentPosition() + Left.getCurrentPosition())/2;// get avg position of the motors
 
         double output = pid.run(avePos);// calculates the output from PID numbers
-        Right.setPower(output);
-        Left.setPower(output);
+        //Right.setPower(0.5);
+        //Left.setPower(-0.5);
+
+        switch (state){
+            case 1:
+                startTime = time;
+                state = 2;
+                break;
+            case 2:
+                Right.setPower(0.5);
+                Left.setPower(-0.5);
+                if (time > 3 ){
+                    Left.setPower(0);
+                    Right.setPower(0);
+                    state = 3;
+                }
+                break;
+            case 3:
+                RightGripper.setPosition(0.75);
+                LeftGripper.setPosition(0.75);
+                state = 4;
+                break;
+            case 4:
+                startTime = time;
+                state = 5;
+                break;
+            case 5:
+                Right.setPower(-0.5);
+                Left.setPower(0.5);
+                if (time > 2) {
+                    Right.setPower(0);
+                    Left.setPower(0);
+                    state = 6;
+                }
+                break;
+            case 6:
+                Right.setPower(.5);
+                Left.setPower(.5);
+                if (time > 4){
+                    Right.setPower(0);
+                    Left.setPower(0);
+                    state = 7;
+                }
+                break;
+            case 7:
+                Right.setPower(.5);
+                Left.setPower(-.5);
+                if (time > 9){
+                    Right.setPower(0);
+                    Left.setPower(0);
+                    state = 8;
+                }
+                break;
+
+        }
+
+
+
+
     }
 }
